72e98732f52d0ebb16167c90633e8a67ff3c96bd
Vaishnavi <gaikwadvaishnavi1000@gmail.com>
2026-04-09 19:32:49 +0530

    default files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import WallCalendar from './components/WallCalendar';
+
+export default function App() {
+  return (
+    <div className="w-full min-h-screen">
+      <WallCalendar />
+    </div>
+  );
+}
